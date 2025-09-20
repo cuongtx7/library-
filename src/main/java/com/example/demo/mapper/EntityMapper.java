6a807d62fe6f5a96b8342b6cce45da370d7afb76
cuongtx <cuongtx7@fpt.com>
2025-09-21 03:22:09 +0700

    lần cam kết đầu tiên

--- src/main/java/com/example/demo/mapper/EntityMapper.java
+++ src/main/java/com/example/demo/mapper/EntityMapper.java
@@ -1,14 +1,2 @@
-package com.example.demo.mapper;
-
-import java.util.List;
-
-public interface EntityMapper <D, E> {
-
-    E toEntity(D dto);
-
-    D toDto(E entity);
-
-    List<E> toEntity(List<D> dtoList);
-
-    List <D> toDto(List<E> entityList);
+package com.example.demo.mapper;public interface EntityMapper {
 }
